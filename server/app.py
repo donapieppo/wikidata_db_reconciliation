7be760748cffd8efcf61f357b3b3d182e23c7f5a
Pietro <donapieppo@yahoo.it>
2024-02-02 15:42:12 +0100

    on

--- server/app.py
+++ server/app.py
@@ -1,22 +1,38 @@
+from flask import Flask, request, jsonify, render_template
 from wikidata_local_reconciliator import WikidataLocalReconciliator
-from flask import Flask, request, jsonify
-
-reconciliator = WikidataLocalReconciliator(db_file='../tmp/wikidata.db')
+import json
 
 app = Flask(__name__)
 
 @app.route('/api/v1/s', methods=['GET'])
-def search():
+def api_search():
     # Get query parameters from the URL
     name = request.args.get('name')
     year = request.args.get('year')
+    year = int(year) if len(year) > 0 else 0
 
-    # Perform your search logic here (replace this with your actual implementation)
-    # For example, just returning the parameters as JSON for demonstration purposes
-    result = {'name': name, 'year': year}
-    result = reconciliator.ask('martin scorsese', 2000, 'film_director')
+    reconciliator = WikidataLocalReconciliator(db_file='../../../tmp/wikidata.db')
+    result = reconciliator.ask(name, year, 'film_director')
 
-    return jsonify(result)
+    return jsonify(json.dumps(result, default=tuple))
+    return jsonify({'wiki_id': result['wiki_id'], 
+                    'label': result['label'],
+                    'year_of_birth': result['year_of_birth']})
+
+@app.route('/search', methods=['POST'])
+def search(): 
+    name = request.form['name']
+    year = int(request.form['year']) if len(request.form['year']) > 0 else 0
+
+    reconciliator = WikidataLocalReconciliator(db_file='../../../tmp/wikidata.db')
+    result = reconciliator.ask(name, year, 'film_director')
+
+    return render_template('search.html', name=name, result=result)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
